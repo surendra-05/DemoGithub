15f1de5b0a7f7195f20828a8c5d26a52983df7c2
HP <HP@DESKTOP-76U8IES>
2025-06-24 20:37:09 +0530

    Second push

---
+++ DemoGit/src/test/java/ContactTest/CreateContact.java
@@ -0,0 +1,10 @@
+package ContactTest;
+
+public class CreateContact {
+
+	public static void main(String[] args) {
+		System.out.println("Contact created Successfully");
+
+		}
+
+}
---
+++ DemoGit/src/test/java/OrganTest/CretaeOrgan.java
@@ -0,0 +1,9 @@
+package OrganTest;
+
+public class CretaeOrgan {
+
+	public static void main(String[] args) {
+		System.out.println("Organisation created successfully");
+	}
+
+}
